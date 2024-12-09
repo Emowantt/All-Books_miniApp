949b8d735cc7d7e80ed0fcfb99d2b1fea2001d42
Wohash <iliav@Emonzo>
2024-12-09 16:05:23 +0300

    style

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -11,9 +11,9 @@ const ProductItem = ({product, className, onAdd}) => {
     return (
         <div className={'product' + className}>
             <div className='img'/>
-            <h2 className='title'>{product.title}</h2>
-            <p className='author'>{product.author}</p>
-            <p className='category'>{product.category}</p>
+            <div className='title'>{product.title}</div>
+            <div className='author'>{product.author}</div>
+            <div className='category'>{product.category}</div>
             <div className='price'>
                 <b>{product.price}</b>
             </div>
